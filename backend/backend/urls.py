880d89fb6444c299644fcabe99bc1d3154f8a8b6
Mohammed Fuad Al-nasheri <70831918+Mohmmde1@users.noreply.github.com>
2025-02-15 18:42:17 +0800

    feat: django-toolbar setup

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from debug_toolbar.toolbar import debug_toolbar_urls
+
 urlpatterns = [
     path("admin/", admin.site.urls),  # Django admin
     path("api/v1/", include("v1.urls")),  # API Version 1 routes
-]
+] + debug_toolbar_urls()
