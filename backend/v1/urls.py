dfaeedf56afed24607fcf11a9b440c70e14d0f4f
Mohammed Fuad Al-nasheri <70831918+Mohmmde1@users.noreply.github.com>
2025-02-24 16:27:50 +0800

    test: test-cases for the auths app

--- backend/v1/urls.py
+++ backend/v1/urls.py
@@ -13,5 +13,4 @@ urlpatterns = [
     path("auth/registration/", include("dj_rest_auth.registration.urls")),
     path("auth/google/", views.GoogleLogin.as_view(), name="google_login"),
     path("faq/", include("faq.urls")),
-    path("scrape/", views.scrape_url, name="scrape-url"),
 ]
